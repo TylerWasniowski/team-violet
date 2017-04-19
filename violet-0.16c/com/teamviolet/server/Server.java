c0536953f3f27db3e857af1b011a6b266cbfcfe3
TylerWasniowski <TylerWasniowski@gmail.com>
2017-04-19 16:01:36 -0700

    Reformatted Server code and added printStackTrace when catching exceptions

--- violet-0.16c/com/teamviolet/server/Server.java
+++ violet-0.16c/com/teamviolet/server/Server.java
@@ -9,54 +9,51 @@ import java.net.Socket;
 
 /*
  * Based off https://www.cs.uic.edu/~troy/spring05/cs450/sockets/EchoServer.java
- */ 
+ */
 public class Server {
-    public static void main(String [] args) throws IOException {
-        ServerSocket serverSocket = null; 
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
 
-        try { 
-             serverSocket = new ServerSocket(10007); 
-            } 
-        catch (IOException e) 
-            { 
-             System.err.println("Could not listen on port: 10007."); 
-             System.exit(1); 
-            } 
+        try {
+            serverSocket = new ServerSocket(10007);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 10007.");
+            e.printStackTrace();
+            System.exit(1);
+        }
 
-        Socket clientSocket = null; 
-        System.out.println ("Waiting for connection.....");
+        Socket clientSocket = null;
+        System.out.println("Waiting for connection.....");
 
-        try { 
-             clientSocket = serverSocket.accept(); 
-            } 
-        catch (IOException e) 
-            { 
-             System.err.println("Accept failed."); 
-             System.exit(1); 
-            } 
+        try {
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.err.println("Accept failed.");
+            e.printStackTrace();
+            System.exit(1);
+        }
 
-        System.out.println ("Connection successful");
-        System.out.println ("Waiting for input.....");
+        System.out.println("Connection successful");
+        System.out.println("Waiting for input.....");
 
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), 
-                                          true); 
-        BufferedReader in = new BufferedReader( 
-                new InputStreamReader( clientSocket.getInputStream())); 
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),
+                true);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
 
-        String inputLine; 
+        String inputLine;
 
-        while ((inputLine = in.readLine()) != null) 
-            { 
-             System.out.println ("Server: " + inputLine); 
-             out.println(inputLine); 
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println("Server: " + inputLine);
+            out.println(inputLine);
 
-             if (inputLine.equals("Bye.")) 
-                 break; 
-            } 
+            if (inputLine.equals("Bye."))
+                break;
+        }
 
-        out.close(); 
-        in.close(); 
-        clientSocket.close(); 
+        out.close();
+        in.close();
+        clientSocket.close();
         serverSocket.close();
     }
 }
