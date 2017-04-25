fd175f5295ffcfb717881c4167429cfceaf40e4c
Chitwant <cheetah@Chitwants-MacBook-Pro.local>
2017-04-25 15:34:49 -0700

    delete previous serialUID

--- violet-0.16c/com/horstmann/violet/Command.java
+++ violet-0.16c/com/horstmann/violet/Command.java
@@ -13,7 +13,6 @@ public class Command implements Serializable {
     private TeamSequenceDiagramGraph.CommandType commandType;
     private List<Object> commandInputs;
     
-    //private static final long serialVersionUID = 1L;
     private static final long serialVersionUID = 42L;
 
     public Command(TeamSequenceDiagramGraph.CommandType commandType, Object... commandInputs) {
